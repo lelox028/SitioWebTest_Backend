b6cb29bd3214f9161cce9d51c6c3b8f9ba4a3bb5
Lelox028 <giulianopascarelli@gmail.com>
2024-12-04 19:52:50 -0300

    Added Database script

---
+++ Database/Create.sql
@@ -0,0 +1,20 @@
+#create DATABASE
+CREATE DATABASE `SitioWebTest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+
+USE SitioWebTest;
+#Create table
+CREATE TABLE `Items` (
+  `id_Items` int(11) NOT NULL,
+  `name_Items` varchar(100) NOT NULL,
+  `status_Items` bit(1) NOT NULL,
+  PRIMARY KEY (`id_Items`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+#Insert test data
+INSERT INTO `Items` (id_Items, name_Items, status_Items) VALUES
+(1,"Texto que se actualice segun variables de React", 1),
+(2,"Links a mi github, linkedin, etc.",0);
+
+#Read new Data
+SELECT *
+FROM Items;
