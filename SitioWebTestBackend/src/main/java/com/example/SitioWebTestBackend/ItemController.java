77b60236ce367e473114761a78b7093106e00d44
Lelox028 <giulianopascarelli@gmail.com>
2024-12-05 22:18:29 -0300

    Implemented a Put endpoint for updating items

--- SitioWebTestBackend/src/main/java/com/example/SitioWebTestBackend/ItemController.java
+++ SitioWebTestBackend/src/main/java/com/example/SitioWebTestBackend/ItemController.java
@@ -1,7 +1,11 @@
 package com.example.SitioWebTestBackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,4 +21,13 @@ public class ItemController {
         return itemService.getAllItems();
     }
 
+    @PutMapping("/item/{id}")
+    public ResponseEntity<Item> updateItem(@PathVariable int id, @RequestBody Item updatedItem){
+        try {
+            Item item = itemService.updateItem(id, updatedItem);
+            return ResponseEntity.ok(item);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- SitioWebTestBackend/src/main/java/com/example/SitioWebTestBackend/ItemService.java
+++ SitioWebTestBackend/src/main/java/com/example/SitioWebTestBackend/ItemService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ItemService {
@@ -13,5 +14,17 @@ public class ItemService {
 
     public List<Item> getAllItems() {
         return itemRepository.findAll();
+    }
+
+    public Item updateItem(int id, Item updatedItem) {
+        Optional<Item> optionalItem = itemRepository.findById(id);
+        if (optionalItem.isPresent()) {
+            Item existingItem = optionalItem.get();
+            existingItem.setName_Items(updatedItem.getName_Items());
+            existingItem.setStatus_Items(updatedItem.getStatus_Items());
+            return itemRepository.save(existingItem);
+        } else {
+            throw new RuntimeException("Item not found with ID: " + id);
+        }
     }
 }
